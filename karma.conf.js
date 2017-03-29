1fc4ac49610d65d26dc59bced2f1a112239f08c4
uniray7 <uniray7@gmail.com>
2017-03-29 12:46:14 +0800

    test

--- karma.conf.js
+++ karma.conf.js
@@ -4,10 +4,34 @@ const webpack = require('webpack');
 const webpackCfg = require('./webpack.config');
 
 const useIframe = process.argv.includes('--iframe');
+const completed = process.argv.includes('--completed');
 
 module.exports = function(config) {
-  const browsers = ['PhantomJS'];
-  const customLaunchers = {};
+  const customLaunchers = !completed ? {} : {
+    // Chrome on Windows
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '49.0',
+      platform: 'Windows 10',
+    },
+    // Firefox on Windows
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '40.0',
+      platform: 'Windows 10',
+    },
+    // IE on Windows
+    'SL_InternetExplorer': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '11.0',
+      platform: 'Windows 10',
+    },
+  };
+  const browsers =
+    !completed ? ['PhantomJS'] : Object.keys(customLaunchers);
 
   config.set({
     basePath: '',
@@ -17,7 +41,7 @@ module.exports = function(config) {
       'test/runner.js'
     ],
     port: 8080,
-    captureTimeout: 60000,
+    captureTimeout: 120000,
     frameworks: [
       'mocha',
       'chai',
@@ -28,26 +52,33 @@ module.exports = function(config) {
       mocha: {},
     },
     singleRun: true,
-    reporters: [
+    reporters: !completed ? [
       'mocha',
       'coverage',
+    ] : [
+      'mocha',
+      'saucelabs',
     ],
     preprocessors: {
       'test/runner.js': [
         'webpack',
       ],
     },
+    concurrency: 5,
     webpack: merge(webpackCfg, {
       plugins: [
         new webpack.DefinePlugin({
           "process.env": {
-            PHANTOM: JSON.stringify(true),
+            PHANTOM: !completed ? JSON.stringify(true) : JSON.stringify(false),
           }
         }),
       ],
     }),
     webpackServer: {
       noInfo: true,
+    },
+    sauceLabs: !completed ? {} : {
+      testName: 'Verpix Viewer Testing',
     },
     coverageReporter: {
       dir: 'coverage/',
--- test/unit/isArrayOfString.spec.js
+++ test/unit/isArrayOfString.spec.js
@@ -7,7 +7,7 @@ describe('isArrayOfString()', () => {
       ['STR'],
       ['str0', 'str1'],
       ['s0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's10'],
-    ].forEach((arr) => expect(isArrayOfString(arr)).to.be.false);
+    ].forEach((arr) => expect(isArrayOfString(arr)).to.be.true);
   });
 
   it('should return false if input is array of string', () => {
